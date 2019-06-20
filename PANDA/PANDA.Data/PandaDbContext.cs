982919afd3cc80d4ef9870e599bc98dae4f8060b
Mihail Tanev <mihail.v.tanev@gmail.com>
2019-06-20 18:48:47 +0200

    added Panda project files

---
+++ PANDA/PANDA.Data/PandaDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace PANDA.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class PandaDbContext : IdentityDbContext
+    {
+        public PandaDbContext(DbContextOptions<PandaDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ PANDA/PANDA.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿namespace PANDA.Web.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using PANDA.Web.ViewModels;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
