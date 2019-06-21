b0d919797bdf6b7e2abc87667bd8285df1c1a8ba
Mihail Tanev <mihail.v.tanev@gmail.com>
2019-06-21 20:47:16 +0200

    added database models

--- PANDA/PANDA.Data/PandaDbContext.cs
+++ PANDA/PANDA.Data/PandaDbContext.cs
@@ -2,8 +2,9 @@
 {
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using PANDA.Models;
 
-    public class PandaDbContext : IdentityDbContext
+    public class PandaDbContext : IdentityDbContext<PandaUser>
     {
         public PandaDbContext(DbContextOptions<PandaDbContext> options)
             : base(options)
